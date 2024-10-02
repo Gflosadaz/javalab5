b694ec35d4d113f303cc7c41e90def493b0e9912
German Losada <200594722@student.georgianc.on.ca>
2024-10-02 14:09:29 -0400

    Add files via upload

---
+++ 09-function-challenges/script.js
@@ -0,0 +1,21 @@
+function getCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+console.log(`The temperature is ${getCelsius(32)} \xB0C`);
+// The temperature is 0 °C
+
+//chalenge 2
+const minMax = (arr) => ({
+    min: Math.min(...arr),
+    max: Math.max(...arr)
+});
+
+console.log(minMax([1, 2, 3, 4, 5]));
+// { min: 1, max: 5 }
+
+//chalenge 3
+(function(length, width) {
+    const area = length * width;
+    console.log(`The area of a rectangle with a length of ${length} and a width of ${width} is ${area}.`);
+})(10, 5);
